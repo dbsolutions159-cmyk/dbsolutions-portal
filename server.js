145883b6d928939134fe3bdf58874991789b61ef
dbsolutions159-cmyk <dbsolutions159@gmail.com>
2026-03-29 03:23:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-require("dotenv").config();   // ✅ पहले
-const mongoose = require("mongoose");
-console.log("MONGO URI:", process.env.MONGO_URI);  // ✅ बाद में
+require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI)
+const mongoose = require("mongoose");
+
+console.log("MONGO URI:", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => console.log("MongoDB Connected ✅"))
 .catch(err => console.log("Mongo Error:", err));
-const express = require("express");
-const cors = require("cors");
-
 const app = express();
 
 app.use(cors());
