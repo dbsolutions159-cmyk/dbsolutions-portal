ba54e321d740891eb4e1721d0d08cdd2d6fa7c27
dbsolutions159-cmyk <dbsolutions159@gmail.com>
2026-03-30 01:31:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,8 @@ Reply: Great 👍 Kis field me job chahte ho?
 });
 
 // ✅ START SERVER
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("🚀 Server running on http://localhost:" + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
