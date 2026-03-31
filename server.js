add2fb5b5ed722892fe9d6bba5de2bd1b3805861
dbsolutions159-cmyk <dbsolutions159@gmail.com>
2026-03-31 11:43:42 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const Job = require("./models/job")
+
 const app = express();
 
 app.use(cors());
